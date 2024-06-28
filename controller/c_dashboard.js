9f1c84327ed56ee29a4a809e079d78a167f964b1
gracealoina <aloinagrace.sitepu@gmail.com>
2024-06-28 22:16:42 +0700

    list user

--- controller/c_dashboard.js
+++ controller/c_dashboard.js
@@ -2,7 +2,8 @@ module.exports=
 {
     index: function(req,res){
         let dataview = {
-            konten: 'dashboard/index'
+            konten: 'dashboard/index',
+            uri_segment: req.path.split('/'),
         }
         res.render('template/struktur',dataview)
     },
